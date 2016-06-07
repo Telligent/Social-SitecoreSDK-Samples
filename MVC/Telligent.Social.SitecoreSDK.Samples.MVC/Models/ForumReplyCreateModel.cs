4617bea7b6258eadce903f2192a612115e36206e
unknown <rsandoval@firmcontact.com>
2016-06-07 10:24:14 -0500

    Updated zimbra references to telligent

---
+++ MVC/Telligent.Social.SitecoreSDK.Samples.MVC/Models/ForumReplyCreateModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Telligent.Social.SitecoreSDK.Samples.MVC.Models
+{
+    public class ForumReplyCreateModel
+    {
+        public int ThreadId { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ MVC/Telligent.Social.SitecoreSDK.Samples.MVC/Models/LikeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Telligent.Social.SitecoreSDK.Samples.MVC.Models
+{
+    public class LikeModel
+    {
+        public LikeModel() { }
+
+        public LikeModel(string contentTypeId, string contentId, dynamic results)
+        {
+            ContentTypeId = contentTypeId;
+            ContentId = contentId;
+            Results = results;
+        }
+
+       public string ContentTypeId { get; set; }
+       public string ContentId { get; set; }
+       public string Url { get; set; }
+       public dynamic Results { get; set; }
+       public bool IsLiked { get; set; }
+    }
+}
